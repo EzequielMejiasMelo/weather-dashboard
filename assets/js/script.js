c4cfa0dd6ac7e56d4ed8c42de9824cdf7402b8e5
Ezequiel Mejias <ezequiel.mejiasmelo@gmail.com>
2022-01-08 21:46:18 -0500

    Finished forecast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,17 +31,19 @@ function initialRender(city) {
 
 function renderCityBtn(event){
     if (event.target.dataset.index === "Submit") {
-        var requestedCity = $(".form-control").val()
-        if (!renderedCities.includes(requestedCity)){
-            var buttonNode = $("<button>")
-                .addClass("btn btn-secondary w-100 m-2 fs-5")
-                .attr("data-index", requestedCity)
-                .text(requestedCity);
-            
-            historyDiv.append(buttonNode);
-            renderedCities.push(requestedCity);
+        var requestedCity = $(".form-control").val();
+        if (requestedCity !== null || requestedCity !== ""){
+            if (!renderedCities.includes(requestedCity)){
+                var buttonNode = $("<button>")
+                    .addClass("btn btn-secondary w-100 m-2 fs-5")
+                    .attr("data-index", requestedCity)
+                    .text(requestedCity);
+                
+                historyDiv.append(buttonNode);
+                renderedCities.push(requestedCity);
+            }
+            findCityCoord(requestedCity);
         }
-        findCityCoord(requestedCity);
     }
     return;
 }
@@ -51,12 +53,10 @@ function findCityCoord(city){
         url: `${geocodeApiUrl}q=${city}&limit=3&appid=${weatherApiKey}`,
         method: "GET"
     }).then(function (response) {
-        console.log("GET request from Geocode API \n-----------")
-        console.log(response);
+        //save city coordinates and pass to weather API
         var latitude = response[0].lat;
         var longitude = response[0].lon;
-        renderCurrentWeather(city, latitude, longitude);
-        renderForecastWeather(city, latitude, longitude);
+        pullWeatherData(city, longitude, latitude);
     });
     return;
 }
@@ -73,6 +73,7 @@ function pullWeatherData(city, longitude, latitude){
 
 function renderCurrentWeather(city, data){
     // current weather data
+    console.log(data);
     var currTemp = data.current.temp;
     var currWind = data.current.wind_speed;
     var currHumid = data.current.humidity;
@@ -81,27 +82,39 @@ function renderCurrentWeather(city, data){
 
     $(".todayDate").text(`${city} (${moment().format("M/D/YYYY")})`);
     $(".todayIcon").attr("src", currentIcon).attr("alt", data.current.weather[0].description);
-    $(".todayTemp").text(currTemp);
-    $(".todayWind").text(currWind + " MPH");
-    $(".todayHumid").text(currHumid);
-    $(".todayUV").text(currentUv);
+    $(".todayTemp").text(`${currTemp} °F`);
+    $(".todayWind").text(`${currWind} MPH`);
+    $(".todayHumid").text(`${currHumid} %`);
+    $(".todayUV").addClass("bg-success rounded text-light p-2").text(currentUv);
 
     return;
 }
 
 function renderForecastWeather(city, data){
+    document.querySelector(".card-list").innerHTML = "";
+
     for (var i=0; i<5; i++){
         var forecastTemp = data.daily[i].temp.day;
         var forecastWind = data.daily[i].wind_speed;
         var forecastHumid = data.daily[i].humidity;
         var forecastIcon = `https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`;
 
-        var cardNode = $("<div>").addClass("card-body bg-dark");
-        var dateTag = $("<h4>").text(moment().add(1, "d").format("M/D/YYYY"));
-        var tempTag = $("<p>").addClass("fs-6").text(`Temp: ${forecastTemp}`);
-        var windTag = $("<p>").addClass("fs-6").text(`Temp: ${forecastWind}`);
-        var humidTag = $("<p>").addClass("fs-6").text(`Temp: ${forecastHumid}`);
-        var iconTag = $("<img>").attr("src", forecastIcon);
+        var cardNode = $("<div>").addClass("card bg-secondary col-2 m-2 text-light");
+        var cardBody = $("<div>").addClass("card-body");
+        var dateTag = $("<h4>").addClass("mb-1").text(moment().add(i + 1, "d").format("M/D/YYYY"));
+        var tempTag = $("<p>").addClass("fs-5").text(`Temp: ${forecastTemp} °F`);
+        var windTag = $("<p>").addClass("fs-5").text(`Temp: ${forecastWind} MPH`);
+        var humidTag = $("<p>").addClass("fs-5").text(`Temp: ${forecastHumid} %`);
+        var iconTag = $("<img>").addClass("mx-auto float-start").attr("src", forecastIcon);
+
+        cardNode.append(dateTag);
+        cardNode.append(iconTag);
+        cardNode.append(tempTag);
+        cardNode.append(windTag);
+        cardNode.append(humidTag);
+
+        cardNode.append(cardBody);
+        $(".card-list").append(cardNode);
 
     }
     return;
